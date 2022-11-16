e3b6b941b6044ae6c2a1d069a6ba97e9531b90ca
Vaisakh174 <101011734+Vaisakh174@users.noreply.github.com>
2022-11-16 15:30:21 +0530

    final

--- app.js
+++ app.js
@@ -23,92 +23,10 @@ app.use(express.static(path.join(__dirname + "/dist/blog-case_study-2")));
 // app.use(express.static("/dist/blog-case_study-2"));
 
 
-// router move to app.js
-// const express = require("express");
-// const router = express.Router();
-// const DATA = require("./models/blogdata")
-
-//get all list (get)
-// app.get('/getall', async (req, res) => {
-
-//     try {
-//         let list = await DATA.find();
-        
-//         console.log(`from get method ${list}`);
-//         res.send(list);
-//     }
-//     catch (error) {
-//         console.log(`error from get method ${error}`);
-
-//     }
-
-// });
-
-
-
-//add data (post)
-// app.post('/post', async (req, res) => {
-
-//     try {
-//         let item = {
-//             blogerName: req.body.blogerName,
-//             blogerImg: req.body.blogerImg,
-//             followCount: req.body.followCount,
-//             articleTitle: req.body.articleTitle,
-//             articleDate: req.body.articleDate,
-//             comment: req.body.comment,
-//             user: req.body.user,
-//             content1: req.body.content1,
-//             content2: req.body.content2,
-//             content3: req.body.content3,
-//             content4: req.body.content4,
-//             content5: req.body.content5,
-//             content6: req.body.content6,
-//             content7: req.body.content7,
-//             content8: req.body.content8,
-//             content9: req.body.content9,
-//             content10: req.body.content10
-//         }
-//         const newdata = new DATA(item);
-//         const savedata = await newdata.save();
-//         console.log(`from post method ${savedata}`);
-//         res.send(savedata);
-
-//     } catch (error) {
-//         console.log(`error from get method ${error}`);
-//     }
-
-// });
-
-
-
-// update data
-// app.put('/update', async (req, res) => {
-
-//     try {
-//         let id = req.body._id;
-//         let item = {
-//             // comment: req.body.comment,
-//             followCount:req.body.followCount
-//         }
-//         console.log("incoming data from update",this.id,this.item);
-
-//         let updatedata = await DATA.findByIdAndUpdate(
-//             { "_id": id },
-//             { $set: item }
-//         );  
-//         console.log(`from put method old data ${updatedata}`);
-//         res.send(updatedata);
-
-//     } catch (error) {
-//         console.log(`error from get method ${error}`);
-//     }
-
-// });
 
 // for api calls
 const api = require("./router/api.js");
-app.use("/apii", api);
+app.use("/api", api);
 
 // for heroku
 app.get('/*', function (req, res) {
@@ -117,7 +35,7 @@ app.get('/*', function (req, res) {
 
 // set port 
 // const port = "api";
-const port = process.env.PORT|| 3000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`........port is now connected at ${port} ........`);
 });
